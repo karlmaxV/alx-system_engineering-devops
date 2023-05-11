a65f2c4527c590c845e735b621646f6bdc58f281
root <karlmaxsonvicki@gmail.com>
2023-05-11 07:04:06 -0700

    new

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
-
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+# puppet script to fix a typo in a configuration wordpress file
+exec { 'fix':
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
 }
